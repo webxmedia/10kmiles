1c40c305d90446e79c71bf52c197975c54667d1a
webxmedia <WEBxMedia@Matts-iMac-2.local>
2014-06-18 00:48:10 +0100

    Testing

--- app/controllers/UsersController.php
+++ app/controllers/UsersController.php
@@ -9,9 +9,8 @@ class UsersController extends \BaseController {
 	 */
 	public function index()
 	{
-		//$users = User::all();
-        //return View::make('users/index', ['users' => $users]);
-        return View::make('users/index');
+		$users = User::all();
+        return View::make('users/index', ['users' => $users]);
 
 	}
 
--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -3,4 +3,10 @@
 @section('content')
     <h1>All users</h1>
 
+    @foreach $users as $user
+
+        <li>{{ $user->username }}</li>
+
+    @endforeach
+
 @stop
