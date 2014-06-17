0ac780522c78b03d8b37266e778b55f97f5c4b10
webxmedia <mattw@cadmium49.mysticky.private>
2014-06-17 17:15:51 +0100

    New navigation

--- app/views/layouts/public.blade.php
+++ app/views/layouts/public.blade.php
@@ -35,13 +35,15 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">Project name</a>
+                <a class="navbar-brand" href="#">10KMiles</a>
             </div>
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav">
                     <li class="active"><a href="#">Home</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="/features">Features</a></li>
+                    <li><a href="/tour">Tour</a></li>
+                    <li><a href="/signup">Sign up</a></li>
+                    <li><a href="/prices">Prices</a></li>
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
                         <ul class="dropdown-menu">
@@ -61,11 +63,6 @@
 
     <!-- Begin page content -->
     <div class="container">
-        <div class="page-header">
-            <h1>Sticky footer with fixed navbar</h1>
-        </div>
-        <p class="lead">Pin a fixed-height footer to the bottom of the viewport in desktop browsers with this custom HTML and CSS. A fixed navbar has been added with <code>padding-top: 60px;</code> on the <code>body > .container</code>.</p>
-        <p>Back to <a href="../sticky-footer">the default sticky footer</a> minus the navbar.</p>
 
         @yield('content')
 
