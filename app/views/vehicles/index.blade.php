fb97ee61a7f11c1511ac977f317d0424faf2b935
webxmedia <mattw@cadmium49.mysticky.private>
2014-06-18 15:46:38 +0100

    Forms

--- app/views/vehicles/index.blade.php
+++ app/views/vehicles/index.blade.php
@@ -5,14 +5,17 @@
 
     <table class="table table-striped table-hover">
         <tr>
+            <th>&nbsp;</td>
             <th>Make</td>
             <th>Model</td>
+            <th>Fuel</td>
             <th>&nbsp;</td>
         </tr>
         @foreach ($users as $user)
             <tr>
+                <td><img src="http://placehold.it/25x25" alt="..." class="img-rounded"></td>
                 <td>{{ $user->username }}</td>
-                <td>{{ $user->username }}</td>
+                <td>Petrol}</td>
                 <td>
                     <button type="button" class="btn btn-danger btn-sm"><i class="fa fa-times"></i></button>&nbsp;
                     <button type="button" class="btn btn-primary btn-sm"><i class="fa fa-pencil"></i></button>
