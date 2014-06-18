b454c2636cff9fd73661e1eaef340ac30e8cac41
webxmedia <mattw@cadmium49.mysticky.private>
2014-06-18 15:35:12 +0100

    Table

--- app/views/vehicles/index.blade.php
+++ app/views/vehicles/index.blade.php
@@ -4,9 +4,16 @@
     <h1>My vehicles</h1>
 
     <table class="table table-striped table-hover">
-    @foreach ($users as $user)
-        <tr><td>{{ $user->username }}</td></tr>
-    @endforeach
+        <tr>
+            <th>Make</td>
+            <th>Model</td>
+        </tr>
+        @foreach ($users as $user)
+            <tr>
+                <td>{{ $user->username }}</td>
+                <td>{{ $user->username }}</td>
+            </tr>
+        @endforeach
     </table>
 
 @stop
