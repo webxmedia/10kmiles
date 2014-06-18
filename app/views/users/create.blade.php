27f355ae173ae26b72b30fff11991c5130801190
webxmedia <mattw@cadmium49.mysticky.private>
2014-06-18 14:06:35 +0100

    test

--- app/views/users/create.blade.php
+++ app/views/users/create.blade.php
@@ -6,12 +6,12 @@
     {{ Form::open(['route' => 'users.store']) }}
         <div>
             {{ Form::label('username', 'Username: ') }}
-            {{ Form::text('username: ') }}
+            {{ Form::text('username') }}
         </div>
 
         <div>
             {{ Form::label('password', 'Password: ') }}
-            {{ Form::password('password: ') }}
+            {{ Form::password('password') }}
         </div>
 
         <div>{{ Form::submit('Create user') }}</div>
