50e550bd15195b7158148551cf6cbc65aeee31ae
webxmedia <mattw@cadmium49.mysticky.private>
2014-06-18 15:58:29 +0100

    Alignment

--- app/views/vehicles/index.blade.php
+++ app/views/vehicles/index.blade.php
@@ -14,14 +14,14 @@
             <th style="width: 130px;">&nbsp;</td>
         </tr>
         @foreach ($users as $user)
-            <tr style="vertical-align:middle;">
-                <td><img src="http://placehold.it/25x25" alt="..." class="img-rounded"></td>
-                <td>{{ $user->username }}</td>
-                <td>{{ $user->username }}</td>
-                <td>Petrol</td>
-                <td>Mileage</td>
-                <td>Cost</td>
-                <td>
+            <tr>
+                <td style="vertical-align:middle;"><img src="http://placehold.it/25x25" alt="..." class="img-rounded"></td>
+                <td style="vertical-align:middle;">{{ $user->username }}</td>
+                <td style="vertical-align:middle;">{{ $user->username }}</td>
+                <td style="vertical-align:middle;">Petrol</td>
+                <td style="vertical-align:middle;">Mileage</td>
+                <td style="vertical-align:middle;">Cost</td>
+                <td style="vertical-align:middle;">
                     <button type="button" class="btn btn-danger btn-sm"><i class="fa fa-times"></i></button>&nbsp;
                     <button type="button" class="btn btn-warning btn-sm"><i class="fa fa-minus-circle"></i></button>&nbsp;
                     <button type="button" class="btn btn-primary btn-sm"><i class="fa fa-pencil"></i></button>
