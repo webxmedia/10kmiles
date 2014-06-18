631d33c0ebb8db603e236be718efc8beddc333ea
webxmedia <mattw@cadmium49.mysticky.private>
2014-06-18 16:08:49 +0100

    Table

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -3,8 +3,26 @@
 @section('content')
     <h1>Users</h1>
 
+<table class="table table-striped table-hover">
+    <tr>
+        <th style="width: 30px;">&nbsp;</td>
+        <th>Username</td>
+        <th>Name</td>
+        <th>Email</td>
+        <th style="width: 130px;">&nbsp;</td>
+    </tr>
     @foreach ($users as $user)
-        <li>{{ $user->username }}</li>
+    <tr>
+        <td style="vertical-align:middle;"><img src="http://placehold.it/25x25" alt="..." class="img-rounded"></td>
+        <td style="vertical-align:middle;">{{ $user->username }}</td>
+        <td style="vertical-align:middle;">{{ $user->username }}</td>
+        <td style="vertical-align:middle;">{{ $user->username }</td>
+        <td style="vertical-align:middle;">
+            <button type="button" class="btn btn-danger btn-sm"><i class="fa fa-times"></i></button>&nbsp;
+            <button type="button" class="btn btn-warning btn-sm"><i class="fa fa-minus-circle"></i></button>&nbsp;
+            <button type="button" class="btn btn-primary btn-sm"><i class="fa fa-pencil"></i></button>
+        </td>
+    </tr>
     @endforeach
-
+</table>
 @stop
