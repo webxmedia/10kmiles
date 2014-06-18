f0df59cd41c37ce7b96da556ee9f1cacd13cceca
webxmedia <mattw@cadmium49.mysticky.private>
2014-06-18 15:41:43 +0100

    Buttons

--- app/views/vehicles/index.blade.php
+++ app/views/vehicles/index.blade.php
@@ -13,7 +13,10 @@
             <tr>
                 <td>{{ $user->username }}</td>
                 <td>{{ $user->username }}</td>
-                <td><button type="button" class="btn btn-danger btn-sm"><i class="fa fa-times"></i></button> <button type="button" class="btn btn-primary btn-sm"><i class="fa fa-pencil"></i></button></td>
+                <td>
+                    <button type="button" class="btn btn-danger btn-sm"><i class="fa fa-times"></i></button>&nbsp;
+                    <button type="button" class="btn btn-primary btn-sm"><i class="fa fa-pencil"></i></button>
+                </td>
             </tr>
         @endforeach
     </table>
