4318d6170e0668bf32dd98b7ab06622d363a82c0
webxmedia <mattw@cadmium49.mysticky.private>
2014-06-18 16:02:49 +0100

    Test

--- app/views/vehicles/index.blade.php
+++ app/views/vehicles/index.blade.php
@@ -19,7 +19,7 @@
                 <td style="vertical-align:middle;">{{ $user->username }}</td>
                 <td style="vertical-align:middle;">{{ $user->username }}</td>
                 <td style="vertical-align:middle;">Petrol</td>
-                <td style="vertical-align:middle;">69,500</td>
+                <td style="vertical-align:middle;">69,501</td>
                 <td style="vertical-align:middle;">£10,000</td>
                 <td style="vertical-align:middle;">
                     <button type="button" class="btn btn-danger btn-sm"><i class="fa fa-times"></i></button>&nbsp;
